e51cec62cc137b901bb3f5d2de8b56413fbab735
matthewhjt <matthewhjt72@gmail.com>
2025-06-25 19:41:17 +0700

    chore: update main.ts config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Cors Option
+  const whitelistUrls: any[] = (
+    process.env.APP_WHITELIST ?? 'http://localhost:3000'
+  ).split(',');
+  const corsOptions = {
+    credentials: true,
+    origin: (origin, callback) => {
+      if (!origin || whitelistUrls.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  };
+  app.enableCors(corsOptions);
+  app.use(json({ limit: '1mb' }));
+  app.use(urlencoded({ extended: true, limit: '1mb' }));
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? process.env.APP_PORT ?? 3001);
 }
 bootstrap();
